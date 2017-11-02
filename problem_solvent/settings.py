99c2c165923945120d6f60e4a5f6b1cc3b974319
Nigel Hardy <nigeljames.12@gmail.com>
2017-11-02 22:17:56 +0000

    urllib works on python3

--- problem_solvent/settings.py
+++ problem_solvent/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 import dj_database_url
 from urllib.parse import urlparse
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
