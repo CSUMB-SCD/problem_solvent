f8fd0fc649368cb75c96ab989a8c78f44173e9b2
Nigel Hardy <nigeljames.12@gmail.com>
2017-11-17 17:49:53 +0000

    Added one more test

--- pages/tests.py
+++ pages/tests.py
@@ -8,9 +8,18 @@ class SimpleTest(TestCase):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
 
-    def test_details(self):
+    def test_home(self):
         # Create an instance of a GET request.
         request = self.factory.get('/')
+        request.user = AnonymousUser()
+
+        # Test my_view() as if it were deployed at /customer/details
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_login(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/login')
         request.user = AnonymousUser()
 
         # Test my_view() as if it were deployed at /customer/details
